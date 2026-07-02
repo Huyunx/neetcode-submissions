e35a6ba0c7999be898dd5ff16358cca8285e7934
Huyunx <136183703+Huyunx@users.noreply.github.com>
2026-07-02 10:10:47 +0100

    Add: binary-tree-maximum-path-sum - submission-4

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-4.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    maxlinecontain={}
+    def maxcontinuoslinecontain(self,root):
+        if(not root):
+            return -1001
+        self.maxlinecontain[root]=max(max(self.maxcontinuoslinecontain(root.left),self.maxcontinuoslinecontain(root.right)),0)+root.val
+        return self.maxlinecontain[root]
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.maxcontinuoslinecontain(root)
+        def maxps(root):
+            maxlineleft=0
+            maxlineright=0
+            if(not root):
+                return -1001
+            if root.left:
+                maxlineleft=max(0,self.maxlinecontain[root.left])
+            if root.right:
+                maxlineright=max(0,self.maxlinecontain[root.right])
+            maxsumwithcurrnode=maxlineleft+maxlineright+root.val
+            return max(max(maxps(root.left),maxps(root.right)), maxsumwithcurrnode)
+        return maxps(root)
