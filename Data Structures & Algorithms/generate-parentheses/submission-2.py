09c1182e31c9c716ee9cce2e5341f8fa6b66ea87
Huyunx <136183703+Huyunx@users.noreply.github.com>
2026-08-11 15:15:37 +0800

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans=[]
+  
+        def dfs(skobi,zbor,num):
+            if(zbor<0):
+                return 
+            opened=(zbor+num)//2
+            if(opened>n or opened-zbor>n):
+                return
+            if(num==2*n and zbor==0):
+                ans.append(skobi)
+            
+            dfs(skobi+'(', zbor+1,num+1)
+            dfs(skobi+')',zbor-1,num+1)
+            return 
+        dfs('',0,0)
+        return ans
